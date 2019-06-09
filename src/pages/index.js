aa7e13c255e0c85f08dd30b3852218afc10f46a8
Pranav <pranavap96@gmail.com>
2019-06-09 17:47:13 +0530

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    hello
+  </div>
+)
+
+
+// 1. Get url - username and project name
+// 2. get shield links
+// 3. get license
+// 4. describe the project - short - long
+// 5. Setup for development
+//   6. external Usage - going to be used in other people code
+// 7. Change Log ?
+//   8. Screen shots / gifs
+// 9. Code of conduct
