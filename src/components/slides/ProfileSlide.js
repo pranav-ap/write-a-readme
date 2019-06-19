b1f59c0c6c661fc2da59edf218370e6258c5c931
Pranav <pranavap96@gmail.com>
2019-06-19 07:43:19 +0530

    minor update

--- src/components/slides/ProfileSlide.js
+++ src/components/slides/ProfileSlide.js
@@ -5,12 +5,16 @@ import Next from '../Next'
 import Styles from './styles/ProfileSlide.module.scss'
 import BaseStyles from './styles/Slide.module.scss'
 
+const onClickNext = () => {
+
+}
+
 const ProfileSlide = () => {
   return (
     <div className={`columns ${BaseStyles.Slide}`} id={Styles.ProfileSlide}>
       <div className="column">
         <h1 className={'is-size-2'}>Profile</h1>
-        <Next/>
+        <Next onClickNext={onClickNext}/>
       </div>
     </div>
   )
