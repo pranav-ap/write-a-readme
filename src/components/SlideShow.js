e03d7b0bc1081f59dbcf8bfba89b5e68b10019ee
Pranav <pranavap96@gmail.com>
2019-06-20 21:26:06 +0530

    built with update

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import { useStoreState } from 'easy-peasy'
+
+import LicenseSlide from './slides/LicenseSlide'
 import BuiltWithSlide from './slides/BuiltWithSlide'
+import ProfileSlide from './slides/ProfileSlide'
+import DescriptionSlide from './slides/DescriptionSlide'
+import SetupDevelopmentSlide from './slides/SetupDevelopmentSlide'
+import DownloadSlide from './slides/DownloadSlide'
 
 import Styles from './styles/SlideShow.module.scss'
 
 const get_slide = (active_slide) => {
-  // if (active_slide === 0) {
-  //   return <LicenseSlide/>
-  // } else if (active_slide === 1) {
-  //   return <ProfileSlide/>
-  // } else if (active_slide === 2) {
-  //   return <DescriptionSlide/>
-  // } else if (active_slide === 3) {
-  //   return <SetupDevelopmentSlide/>
-  // } else if (active_slide === 4) {
+  if (active_slide === 0) {
+    return <ProfileSlide/>
+  } else if (active_slide === 1) {
+    return <LicenseSlide/>
+  } else if (active_slide === 2) {
+    return <DescriptionSlide/>
+  } else if (active_slide === 3) {
+    return <SetupDevelopmentSlide/>
+  } else if (active_slide === 4) {
     return <BuiltWithSlide/>
-  // } else {
-  //   return <DownloadSlide/>
-  // }
+  } else {
+    return <DownloadSlide/>
+  }
 }
 
 const SlideShow = () => {
