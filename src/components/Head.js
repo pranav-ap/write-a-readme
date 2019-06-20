0af6ed9cc818c8460a54311d95f9aa584a85c02a
Pranav <pranavap96@gmail.com>
2019-06-20 21:18:13 +0530

    built with update

--- src/components/Head.js
+++ src/components/Head.js
@@ -18,6 +18,8 @@ const Head = ({ title }) => {
       <script src={"https://use.fontawesome.com/5f6704beb1.js"}/>
       <link rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bulma-checkradio@1.1.0/dist/css/bulma-checkradio.min.css"/>
+      <link rel="stylesheet"
+            href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"/>
     </Helmet>
   )
 }
--- src/components/slides/BuiltWithSlide.js
+++ src/components/slides/BuiltWithSlide.js
@@ -11,6 +11,25 @@ const BuiltWithSlide = () => {
     <div className={`columns ${BaseStyles.Slide}`} id={Styles.BuiltWithSlide}>
       <div className="column">
         <Title title={'What tools did you use?'}/>
+
+        <div id={Styles.Collection}>
+          <i className="devicon-babel-plain colored"></i>
+
+          <i className="devicon-react-original-wordmark colored"></i>
+
+          <i className="devicon-sass-original colored"></i>
+
+          <i className="devicon-express-original-wordmark colored"></i>
+
+          <i className="devicon-javascript-plain colored"></i>
+
+          <i className="devicon-mocha-plain colored"></i>
+
+          <i className="devicon-mongodb-plain-wordmark colored"></i>
+
+
+        </div>
+
         <Next/>
       </div>
     </div>
