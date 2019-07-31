854a5739c4d6651662c773e30797b637288c2df0
Pranav <pranavap96@gmail.com>
2019-07-31 21:01:11 +0530

    basics to generate md

--- src/components/slides/DownloadSlide.js
+++ src/components/slides/DownloadSlide.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useStoreState } from 'easy-peasy'
+import { saveAs } from 'file-saver'
 
 import Download from './../../../assets/download.svg'
 
@@ -13,11 +14,22 @@ const DownloadSlide = () => {
   const dev_setup = useStoreState(state => state.dev_setup.content)
   const tools = useStoreState(state => state.built_with.tools)
 
-  console.log(repo)
-  console.log(license)
-  console.log(description)
-  console.log(dev_setup)
-  console.log(tools)
+  const click_download = () => {
+    console.log(repo)
+    console.log(license)
+    console.log(description)
+    console.log(dev_setup)
+    console.log(tools)
+
+    const blob = new Blob([
+      '#', repo, '\n',
+      license,
+      description,
+      dev_setup,
+      tools,
+    ], { type: 'text/markdown;charset=utf-8' })
+    saveAs(blob, 'README.md')
+  }
 
   return (
     <div id={Styles.DownloadSlide} className={BaseStyles.Slide}>
@@ -28,7 +40,10 @@ const DownloadSlide = () => {
           <button className={`button is-medium is-primary is-outlined ${Styles.Button}`}>Preview</button>
         </p>
         <p className="control">
-          <button className={`button is-medium is-info ${Styles.Button}`}>Download</button>
+          <button
+            className={`button is-medium is-info ${Styles.Button}`}
+            onClick={() => click_download()}>Download
+          </button>
         </p>
       </div>
     </div>
